db0ff526d142424af8b8030c2265c40eee534a9b
백준석 <1001229bjs@gmail.com>
2024-09-02 22:55:29 +0900

    EVcharge.ino
    이 코드는 아두이노 보드를 사용하여 블루투스 모듈(HC-06)과 스텝 모터를 제어하는 프로그램입니다. 스마트폰이나 PC와의 통신을 통해 명령을 수신하고, 이를 기반으로 모터를 회전시키거나 LED를 제어하는 역할을 합니다.

--- EVcharge.ino
+++ EVcharge.ino
@@ -1,14 +1,34 @@
+#include <SoftwareSerial.h>
+#include <Stepper.h>
+
+SoftwareSerial HC06(2, 3);
+
+
+const int stepsPerRevolution = 2048;
+
+Stepper myStepper(stepsPerRevolution,11,9,10,8); 
+
+
+
+void setup() {
+  Serial.begin(9600);
+  HC06.begin(9600);
+  myStepper.setSpeed(15); 
+  pinMode(7,OUTPUT);
+}
+
+void loop() {
+  if(HC06.available()){
+    String text = HC06.readStringUntil(0x0A);
     if(text == "welcome!"){
       Serial.println("모터쪽으로 가는 신호");
       myStepper.step(-40960);
-      delay(1000);
       delay(5000);
       digitalWrite(7,HIGH);
     }
     else if(text == "goodbye!"){
       Serial.println("모터에서 멀어지는 신호");
       myStepper.step(40960);
-      delay(1000);
       delay(5000);
       digitalWrite(7,HIGH);
     }
@@ -19,6 +39,7 @@
     }
     Serial.println(text);
   }
+
   if(Serial.available()){
     String text = Serial.readStringUntil(0x0A);
     HC06.println(text);
